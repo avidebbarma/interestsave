3b38f0a0d431c45713091670b56537c4e4ed0646
Abhijit Debbarma <avizeet1.debbarma@gmail.com>
2023-08-25 17:44:10 +0530

    third

--- interestsave/settings.py
+++ interestsave/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -134,3 +136,8 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR,"media/user_upload")
 
 MEDIA_URL = '/media/'
+
+
+CSRF_TRUSTED_ORIGINS = ['https://web-production-0017.up.railway.app/']
+
+CSRF_TRUSTED_ORIGINS = ['https://*.web-production-0017.up.railway.app/']
